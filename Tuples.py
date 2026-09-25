de8be1c9fdf834628eda26e3cc790efb6d115bf3
terseer333 <terseerterese4@gmail.com>
2026-09-25 09:22:50 +0100

    python

---
+++ Tuples.py
@@ -0,0 +1,5 @@
+#immutable list is called a tuple.
+dimensions = (40,50,70, 80)
+print(dimensions[0])
+print(dimensions[1])
+print(dimensions[2])
---
+++ test.py
@@ -0,0 +1,5 @@
+#A ticket should cost 7 units. The developer expects three tickets to produce the integer 21.
+
+def ticket_total(price, quantity):
+    total = ticket_total(7 * 3)
+    print(total)
